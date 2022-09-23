b02d9898d7391052764439039f3738229541a02c
Kenzie MacDougal <kenzie@macdougal.com>
2022-09-22 22:50:08 -0700

    Update Game.unity

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject wolf;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        
+    }
+}
