0ce232e8058b5c52163eee5d4b4fe9d758a8ef22
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2022-09-22 19:50:16 -0700

    lamp music

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private AudioSource[] musics;
+    private AudioSource audio;
+    
     [SerializeField] private PlayerController _playerController;
     public static PlayerController player;
     
@@ -13,6 +16,9 @@ public class GameManager : MonoBehaviour
     public static FuelUI fuelUI;
     
     private List<Lamp> lamps;
+    private List<Lamp> counterLamps;
+
+    private int musicType = 0;
 
     public static float time = 1920f;
     [SerializeField] private ClockUI clock;
@@ -29,6 +35,7 @@ public class GameManager : MonoBehaviour
         if(clock == null) clock = FindObjectOfType<ClockUI>();
 
         lamps = FindObjectsOfType<Lamp>().ToList();
+        counterLamps = FindObjectsOfType<Lamp>().ToList();
     }
 
     // Update is called once per frame
@@ -37,12 +44,49 @@ public class GameManager : MonoBehaviour
         time -= Time.deltaTime;
         clock.UpdateSprite((int) Mathf.Floor(time/240));
 
+        if (player.health > 0)
+        {
+            bool inner = false;
+            bool outer = false;
+            
+            foreach (Lamp l in lamps)
+            {
+                Debug.Log(l.inInnerRadius);
+                Debug.Log(l.inOuterRadius);
+                if (l.inOuterRadius) outer = true;
+                if (l.inInnerRadius) inner = true;
+            }
+
+            if (inner)
+            {
+                musicType = 1;
+            }else if (outer)
+            {
+                musicType = 0;
+            }else
+            {
+                musicType = 2;
+            }
+        }
+
+        for (int i = 1; i < musics.Length; i++)
+        {
+            if (i == musicType)
+            {
+                musics[i].volume = Mathf.Lerp(musics[i].volume, 1, Time.deltaTime * 0.1f);
+            }
+            else
+            {
+                musics[i].volume = Mathf.Lerp(musics[i].volume, 0, Time.deltaTime * 2f);
+            }
+        }
+
         if (time < 0)
         {
             SceneManager.LoadScene("TimeUp");
         }
 
-        if (lamps.Count <= 0)
+        if (counterLamps.Count <= 0)
         {
             SceneManager.LoadScene("Win");
         }
@@ -50,10 +94,9 @@ public class GameManager : MonoBehaviour
 
     public void updateLamps(Lamp l)
     {
-        if (lamps.Contains(l))
+        if (counterLamps.Contains(l))
         {
-            Debug.Log("test");
-            lamps.Remove(l);
+            counterLamps.Remove(l);
         }
     }
 }
